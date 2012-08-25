5a040c2d9658286a3b1bee4f267ad21eccd36e50
Jason Coward <jason@opengeek.com>
2012-08-25 11:28:53 -0600

    Add skipTV and skipIfTagsRemain properties
    skipTV - skip by the specified TV name if the value is non-empty for the Resource
    skipIfTagsRemain - skips Resources that have tags remaining in the cacheable content if enabled

--- _build/properties.inc.php
+++ _build/properties.inc.php
@@ -3,6 +3,20 @@
  * Default properties for the StaticCache Plugin
  */
 $properties = array(
+    array(
+        'name' => 'skipIfTagsRemain',
+        'desc' => 'Skip Resources that have tags remaining in the content that is being cached for the Resource.',
+        'type' => 'combo-boolean',
+        'options' => '',
+        'value' => false,
+    ),
+    array(
+        'name' => 'skipTV',
+        'desc' => 'Skip Resources that have a non-empty value in the Template Variable specified by name. Leave empty to only skip non-cacheable Resources.',
+        'type' => 'textfield',
+        'options' => '',
+        'value' => '',
+    ),
 );
 
 return $properties;
